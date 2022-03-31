9a02cec6d5320544a41e2260ecae7301dd5dba4b
llsc12 <42747613+llsc12@users.noreply.github.com>
2022-03-31 23:29:39 +0100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by llsc12 on 31/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/wordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordleApp.swift
+//  Shared
+//
+//  Created by llsc12 on 31/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct wordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
